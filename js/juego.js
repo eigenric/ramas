688a7fb521927618d6963ba076fb387957165c66
Ricardo Ruiz <riki319@gmail.com>
2014-06-23 17:44:38 +0200

    Scripts

---
+++ js/juego.js
@@ -0,0 +1,12 @@
+var num = Random.Randint(0,5);
+var num2 = Random.Radint(0,5);
+
+if (num == num2) {
+	console.log("Coinciden!");
+} else if (num < num2) {
+	 console.log("El primer número es menor que el segundo");
+} else {
+	console.log("El primer número es mayor que el segundo");
+
+
+console.log("Gracias por jugar!");
