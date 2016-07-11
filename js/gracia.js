4f44b457545eb4335a737ecedad3183b294564d5
Ricardo Ruiz <riki319@gmail.com>
2016-07-11 17:56:19 +0200

    Button game added

--- js/gracia.js
+++
@@ -1,3 +0,0 @@
-var chiste = "Ahí va un chiste, ¿Qué le pregunta un pato a un francés?..... Quoi? Quoi?";
-
-console.log(chiste);
--- js/juego.js
+++ js/juego.js
@@ -1,12 +1,41 @@
-var num = Random.Randint(0,5);
-var num2 = Random.Radint(0,5);
+var randInt = function(num) {
 
-if (num == num2) {
-	console.log("Coinciden!");
-} else if (num < num2) {
-	 console.log("El primer número es menor que el segundo");
-} else {
-	console.log("El primer número es mayor que el segundo");
+	return Math.round(Math.random()*num)
+
+}
+
+var juego = function() {
+
+	var first = randInt(10);
+	var second = randInt(10);
+
+	if (first == second) {
+
+		console.log(first + " es igual que " + second);
+		console.log("Asombrosa coincidencia");
+
+	} else if (first < second) {
+	
+		console.log(first + " es menor que " + second);
+		console.log("Normal...");
+
+	} else {
 
 
-console.log("Gracias por jugar!");
+		console.log(first + " es mayor que " + second);
+		console.log("Asquerosamente increible, no crees?");
+	}
+
+}
+
+
+
+window.onload = function() {
+
+	document.getElementById("juego").onclick = function {
+
+		juego();
+
+	}
+
+}
